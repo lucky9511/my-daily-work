f181a6276b1708462e67bacdb05fda40d41560af
lucky9511 <lk0716704@gmail.com>
2026-05-01 16:58:45 +0530

    file

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,6 +1,8 @@
 import User from '../model/User.model.js';
 import crypto from 'crypto';
 import nodemailer from 'nodemailer';
+import bcrypt from "bcryptjs";  
+import jwt from 'jsonwebtoken';
 
 const registerUser = async (req, res) => {
  //get data
@@ -104,4 +106,40 @@ res.status(200).json({
     success: true,
 });
 };
-export { registerUser,verifyUser };
+
+const login= async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({
+            message: 'Please provide all required fields',
+        });
+    }
+    
+    try { 
+         const user = await User.findOne({ email });
+         if (!user) {
+             return res.status(400).json({
+                 message: 'Invalid credentials',
+             });
+         }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({
+                message: 'Invalid credentials',
+            });
+        }
+      const token = jwt.sign(
+    { userId: user._id, role: user.role },
+    "shhhhh", // secret
+    { expiresIn: '24h' } // options object
+      );
+  
+    res.cookie('token', token, { httpOnly: true, 
+        secure: process.env.NODE_ENV === 'production' });
+} catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
+}
+};
+
+export { registerUser, verifyUser, login };
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import db from './utils/db.js';
-
+import cookieParser from 'cookie-parser';
 //import all routes
 import userRoutes from './routes/user.routes.js';
 
@@ -21,6 +21,7 @@ cors({
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const port = process.env.PORT || 3000;
 
